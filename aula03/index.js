854fba4ba390dfcbf531346f6182a946faf6cb20
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-11-07 09:56:25 -0300

    aulas iniciais

---
+++ aula03/index.js
@@ -0,0 +1,5 @@
+console.log('Hello World!'); //string
+console.log('"Hello World!"');
+console.log(`'Hello' "World!"`)
+
+console.log(123456, 15645512.8554545121);
---
+++ aula04/comentario.js
@@ -0,0 +1,6 @@
+// Comentários
+console.log('olá mundo'); //comentário novo
+console.log('Linha 1')
+//Comentário
+console.log('Linha 2')
+console.log('Linha 3')
