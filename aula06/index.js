118ff6095ff30a020dbc89a279061bfa2a05e1ca
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-11-07 11:53:16 -0300

    aula 07

--- aula06/index.js
+++ aula06/index.js
@@ -5,3 +5,11 @@ console.log('Em 2000', nome, 'conheceu Maria');
 console.log(nome, 'casou-se com  Maria em 2012.');
 console.log('Maria teve 1 filho com ', nome, 'em 2015.');
 console.log('O Filho de', nome, 'se chama Eduardo');
+
+//Não se cria variáveis com palavras reservadas.
+//Variáveis precisam ter nome significativos.
+//Não pode começar com número.
+//Não pode conter espaços ou traços.
+//Utilizamos camelCase.
+//Case-sensitive (maiscúlo e miniscúlo influem).
+//Não utilize 'var' utiliza 'let'.
