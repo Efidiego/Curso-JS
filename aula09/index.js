0a539771453e363e1d751449b24bfa652a64ac6e
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-11-08 12:28:43 -0300

    aula10

---
+++ aula09/index.js
@@ -0,0 +1,8 @@
+var nome = 'fernando'
+var nome = 'fernando'
+nome = 'Fernando' 
+
+/*não usar sem Var/COST/LET. 
+Sempre especificar! Lembrando que VAR não se usa mais */
+
+console.log(nome);
---
+++ aula10/idnex.js
@@ -0,0 +1,10 @@
+//Dados Primitivos: String, number, undefined, null, boolean, symbol.
+//Tipos de Datos: 1) Tipo primitivo; 2) Objeto (Arrays, Functions);
+const nome = 'Fernando' //string
+const nome1 = "Fernando" //string
+const nome2 = `Fernando` //string
+const num1 = 10; //number
+const num2 = 10.52; // number
+let nomeAluno; // undefines = não aponta a local nenhum na memória
+let sobrenomeAluno = null; // Nulo -> não aponta pra nenhum local da memória
+const aprovado = false; // Boolean = true ou false (lógico)
