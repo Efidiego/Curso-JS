756a30743446dff3270f3eef92969c45cfec5e95
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-11-09 11:55:54 -0300

    aula11

---
+++ aula11/index.js
@@ -0,0 +1,15 @@
+/*
+      Aritméticos
+- + Adição / Concatenação
+- - Subtração
+- / Divisão 
+- * Multiplicação
+- ** Pontecialização
+- % Resto da Dvivisão
+
+*/
+
+// Exemplo de adição:
+let x = 10;
+let y = 3;
+console.log(x%y);
