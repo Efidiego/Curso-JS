3220e5cebde5ed496157129d00c5e51c10226650
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-12-04 10:26:17 -0300

    aula 17

---
+++ aula17/index.js
@@ -0,0 +1,35 @@
+/*
+function saudacao(nome) {
+   console.log(`Good Morning ${nome}!`);
+   return 123456;
+    //função para mostrar a saudação do usuario
+}
+const variavel = saudacao('Fernando')
+console.log(variavel);
+*/
+
+/*function saudacao(nome) {
+  return `Good Morning ${nome}!`;
+}
+
+const variavel = saudacao('Fernando')
+console.log(variavel);
+*/
+
+/*function soma (x,y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log (soma(2, 2));
+console.log (soma (3, 1));
+console.log (soma(5, 10));
+*/
+
+const raiz = (n) => {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(12));
+console.log(raiz(27));
