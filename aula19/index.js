3bcdccec5497ffde8d236815e6b8135e6fe93805
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-12-08 11:05:23 -0300

    aula19/20

--- aula19/index.js
+++ aula19/index.js
@@ -1,3 +1,42 @@
 /*
+Primitivos -  string, number, booleans, undefined,
+null (bigint, symbol) - valor
 
+Referência (mutável) - array, object , function
 */
+
+
+//         01234
+
+/*let a = 'A';
+let b = a; //Cópia
+console.log (a, b);
+
+a =  'outra coisa';
+console.log(a,b);
+*/
+
+/*let a = [1, 2, 3];
+let b = a;
+let c= b;
+
+console.log (a, b);
+
+a.push(4);
+console.log(a, b);
+
+b.pop();
+console.log(a, b);
+
+console.log(c);
+*/
+const a = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+};
+
+const b = {...a};
+
+a.nome = 'João';
+console.log(a);
+console.log(b);
