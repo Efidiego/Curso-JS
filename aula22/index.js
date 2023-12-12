7ca52cfbaf26b0e8fbaa3f448d9bc79f0b4e9b25
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-12-12 13:05:13 -0300

    Create index.js

---
+++ aula22/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR  -> OU
+!  -> NOT -> NÃO
+
+*/
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || true || false;
+//console.log(expressaoOr);
+
+/*
+const usuario = 'Fernando'; // form usuário digitou
+const senha = '808080'; // form usuário digitou
+
+const vaiLogar =  usuario ==='Fernando' && senha === '808080';
+console.log(vaiLogar);
+*/
+console.log(!true);
