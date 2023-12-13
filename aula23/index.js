f7085125a3c6e6788baba8b8ad4a2c510dc9db2a
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-12-13 11:17:11 -0300

    aula23

---
+++ aula23/index.js
@@ -0,0 +1,22 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true && false -> vai retornar "o valor verdadeiro"
+
+FALSY
+*false
+0
+'' "" ``
+Null/ undefined
+NaN
+*/
+
+/*
+function falaOi () {
+    return'Oi';
+
+}
+const vaiExecutar = 'Fernando';
+
+console.log(vaiExecutar && falaOi());
+*/
+console.log (0 || false || null || 'Fernando Diego' || true);
