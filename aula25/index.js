aa563b6d792bc3ff1e79ee21a1748bd4591be996
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2024-01-03 11:31:37 -0300

    aula 25

---
+++ aula25/index.js
@@ -0,0 +1,15 @@
+const numero = 10;
+
+// se isso ocorrer (numero >= 0 && numero <= 5) faça isso (código)
+//se não faça isso (o código)
+
+if (numero >= 0 && numero <= 5) {
+    console.log ('Sim, o número é maior ou igual a zero.')
+} else if  (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8.');
+} else if (numero >= 9 && numero <= 11)  { // verdadeiro
+    console.log('O Numero está entre 9 e 11.')
+} else {
+    console.log('O número não está entre 0 e 11.')
+}
+
